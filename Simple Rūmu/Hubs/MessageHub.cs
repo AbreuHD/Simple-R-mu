8d9d2b50afc3fca60127057f7c269c53b9b94c88
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2022-12-01 15:58:20 -0400

    Add project files.

---
+++ Simple Rūmu/Hubs/MessageHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Simple_Rūmu.Hubs
+{
+	public class MessageHub : Hub
+	{
+        public async Task SendStatus(int status)
+        {
+            await Clients.Others.SendAsync("MovieStatus", status);
+        }
+
+        public async Task SendTime(double time)
+        {
+            await Clients.Others.SendAsync("MovieTime", time);
+        }
+
+        public async Task SendMessage(string msg)
+        {
+            await Clients.Others.SendAsync("Messages", msg);
+        }
+    }
+}
---
+++ Simple Rūmu/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿"use strict";
+const player = new Plyr('video', { captions: { active: true } });
+var connection = new signalR.HubConnectionBuilder().withUrl("/movieHub").build();
+
+// Expose player so it can be used from the console
+window.player = player;
+
+
+//Start Connection
+connection.start().then(function () {
+    console.log("Connected");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+//Get Connection Messages
+connection.on("MovieStatus", function (status) {
+    if (status == 0) {
+        player.pause();
+    }
+    else if (status == 1){
+        player.play();
+    }
+});
+
+connection.on("MovieTime", function (time) {
+    if ((player.currentTime - time) > 5 || 5 < (time - player.currentTime)) {
+        player.currentTime = time;
+    }
+});
+
+
+//Send Connection Messages
+function sendMovieMessage(data) {
+    connection.invoke("SendStatus", data).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+function sendMovieTime(data) {
+    connection.invoke("SendTime", data).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+player.on('pause', (event) => {
+    sendMovieMessage(0);
+});
+
+
+player.on('playing', (event) => {
+    sendMovieMessage(1);
+});
+
+player.on('timeupdate', (event) => {
+    sendMovieTime(player.currentTime)
+});
